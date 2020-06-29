24995cc7e83ebae652d979b8eba003460ec51f90
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-06-29 10:33:17 +0200

    fix bugs

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
-const Book {
-    constructor(title, author, isbn){
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
-class UI{
-    addBookToList(book){
-        const list = document.querySelector('#book__list');
-        const row = document.createElement('tr');
-        row.innerHTML= `
+class UI {
+  addBookToList(book) {
+    const list = document.querySelector("#book__list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td><a href="#" class="delete">X</a></td>
         `;
-    }
     list.appendChild(row);
+  }
 }
 
+document.querySelector("#form").addEventListener("submit", function (e) {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
 
+  //instantiate Book and UI
+  const book = new Book(title, author, isbn);
 
-document.querySelector('.form').addEventListener('submit', function(e){
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
+  const ui = new UI();
 
-    //instantiate Book and UI
-    const book = new Book(title, author, isbn);
+  //validation
+  if (title == "" || author === "" || isbn === "") {
+    //show alert
+  } else {
+    //add book to list
+    ui.addBookToList(book);
+  }
 
-    const ui = new UI();
-
-    //validation
-    if(title==''||aithor===''||isbn===''){
-        //show alert
-    }else{
-        //add book to list
-        ui.addBookToList(book);
-      
-    }
-
-    
-    e.preventDefault();
+  e.preventDefault();
 });
